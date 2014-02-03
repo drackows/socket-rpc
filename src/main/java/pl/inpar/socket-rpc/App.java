970799db433c84cae4fc3b3c79df604e8be5913b
Dariusz Raćkowski <drackows@gmail.com>
2014-02-03 22:24:27 +0100

    lets start this :)

---
+++ src/main/java/pl/inpar/socket-rpc/App.java
@@ -0,0 +1,13 @@
+package pl.inpar.socket-rpc;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
